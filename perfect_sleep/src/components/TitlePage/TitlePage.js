4e5cf6ea24d49dd91d3da6a7b0b153f201ea4cba
Emily X <122341479+emiilyxiia@users.noreply.github.com>
2024-04-16 19:02:52 -0400

    added fade

--- perfect_sleep/src/components/TitlePage/TitlePage.js
+++ perfect_sleep/src/components/TitlePage/TitlePage.js
@@ -6,7 +6,7 @@ const TitlePage = () => {
   return (
     <div className="title-page">
       <img src={titlePage} alt="SlumberStats - Plotting Your Path to Quality Sleep" className="background-image"/>
-      <div className="white-bar"></div>
+      <div className="bar-trans"></div>
     </div>
 
   );
