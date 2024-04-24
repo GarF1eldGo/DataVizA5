2387f7476529771f2b63217f17c10f339173eaa6
Emily X <122341479+emiilyxiia@users.noreply.github.com>
2024-04-24 14:17:58 -0400

    merge

--- perfect_sleep/src/components/introScrollama/SleepingGirl.js
+++ perfect_sleep/src/components/introScrollama/SleepingGirl.js
@@ -1,20 +1,9 @@
 import React, {useState, useEffect} from "react";
 import * as d3 from "d3";
 import Component1 from "./NarrativeComponent/Component1";
-<<<<<<< HEAD
 import BoxPlot from "../boxPlot/BoxPlot";
-
-const Component2 = () => {
-    return (
-        <div>
-            <h1>Component2</h1>
-        </div>
-    );
-}
-=======
 import Component2 from "./NarrativeComponent/Component2";
 import Component3 from "./NarrativeComponent/Component3";
->>>>>>> 4b62808 (scroll)
 
 const SleepingGirl = (props) => {
     const [avgSleep, setAvgSleep] = useState(0);
