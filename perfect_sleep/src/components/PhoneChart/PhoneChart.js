ef7e3438ad4466de26959cbca32d318cbe26d229
Kentrie <kt2848@columbia.edu>
2024-04-24 22:19:30 -0400

    Added Phone Reach bar chart

--- perfect_sleep/src/components/PhoneChart/PhoneChart.js
+++ perfect_sleep/src/components/PhoneChart/PhoneChart.js
@@ -4,7 +4,8 @@ import './PhoneChart.css';
 
 const PhoneChart = () => {
 
-    const svgRef = useRef();
+    const svgRef1 = useRef();
+    const svgRef2 = useRef();
     const containerRef = useRef();
     const [data, setData] = useState(null);
   
@@ -23,6 +24,17 @@ const PhoneChart = () => {
             })
         .then(d3.csvParse)
         .then(data => {
+            // Calculate the counts of each "Quality of Sleep" and "Sleep Duration" value
+            const countPhoneTime = d3.rollups(data, v => v.length, d => d['PhoneTime']);
+            const countPhoneReach = d3.rollups(data, v => v.length, d => d['PhoneReach']);
+            const countMapTime= new Map(countPhoneTime);
+            const countMapReach = new Map(countPhoneReach);
+
+            // Add a new property to each data point for the count
+            data.forEach(d => {
+                d.countPhoneTime = countMapTime.get(d['PhoneTime']);
+                d.countPhoneReach = countMapReach.get(d['PhoneReach']);
+            });
             setData(data);
         })
         .catch(error => console.error('There has been a problem with your fetch operation:', error));
@@ -31,56 +43,75 @@ const PhoneChart = () => {
   
     useEffect(() => {
 
-        if (!data) return;
+    if (!data) return;
 
-    const margin = { top: 60, right: 10, bottom: 90, left: 90 };
-    const width = 500 - margin.left - margin.right;
-    const height = 450 - margin.top - margin.bottom;
+    const margin = { top: 60, right: 10, bottom: 90, left: 250 };
+    const width = 650 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
 
-    const svg = d3.select(svgRef.current).attr('width', width + margin.left + margin.right)
+    const svg1 = d3.select(svgRef1.current)
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    const svg2 = d3.select(svgRef2.current)
+        .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
         .append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
 
     // Aggregate data based on "PhoneTime" categories
-    const aggregatedData = d3.rollup(
+    const aggregatedData1 = d3.rollup(
         data,
         v => d3.sum(v, d => +d['Tired']), // Sum up the "Tired" values for each category
         d => d.PhoneTime // Group by "PhoneTime" categories
     );
 
-    console.log("Aggregated Data: ", aggregatedData);
+    // Aggregate data based on "PhoneReach" categories
+    const aggregatedData2 = d3.rollup(
+        data,
+        v => d3.sum(v, d => +d['Tired']), // Sum up the "Tired" values for each category
+        d => d.PhoneReach // Group by "PhoneTime" categories
+    );
+
+    console.log("Aggregated Data: ", aggregatedData1);
 
     const x = d3.scaleBand()
         .domain(['Yes', 'No'])
         .range([0, width])
         .padding(0.1);
 
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(aggregatedData.values())]) // Use the max value of aggregated data
+    const y1 = d3.scaleLinear()
+        .domain([0, d3.max(aggregatedData1.values())]) // Use the max value of aggregated data
         .nice()
         .range([height, 0]);
 
-    svg.append('g')
+    const y2 = d3.scaleLinear()
+        .domain([0, d3.max(aggregatedData2.values())]) // Use the max value of aggregated data
+        .nice()
+        .range([height, 0]);
+
+    svg1.append('g')
         .attr('transform', `translate(0,${height})`)
         .call(d3.axisBottom(x));
 
-    svg.append('g')
-        .call(d3.axisLeft(y));
+    svg1.append('g')
+        .call(d3.axisLeft(y1));
 
-    svg.selectAll('.mybar')
-        .data(aggregatedData)
+    svg1.selectAll('.mybar')
+        .data(aggregatedData1)
         .enter().append('rect')
         .attr('class', 'mybar')
         .attr('x', (d) => x(d[0]))
-        .attr('y', (d) => y(d[1]))
+        .attr('y', (d) => y1(d[1]))
         .attr('width', x.bandwidth())
-        .attr('height', (d) => height - y(d[1]))
+        .attr('height', (d) => height - y1(d[1]))
         .attr('fill', "#abd9e9");
 
     // Graph title
-    svg.append('text')
-        .attr('x', margin.left - 20)
+    svg1.append('text')
+        .attr('x', margin.left - 225)
         .attr('y', margin.top - 80)
         .attr('text-anchor', 'left')
         .style('font-size', '16px')
@@ -88,11 +119,11 @@ const PhoneChart = () => {
         .text('Surveyed Tiredness Based on Phone Usage');
 
     // Y axis label
-    svg.append("text")
+    svg1.append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 0 - margin.left)
         .attr("x", 0 - (height / 2))
-        .attr("dy", "2em")
+        .attr("dy", "12em")
         .style("text-anchor", "middle")
         .style("fill", "white")
         .text("Total Tiredness");
@@ -107,22 +138,71 @@ const PhoneChart = () => {
     */
 
     // X axis label
-    svg.append('text')
-        .attr('x', width / 2.5 + margin.left/2)
+    svg1.append('text')
+        .attr('x', width / 6 + margin.left/2)
         .attr('y', height-margin.bottom+150)
         .attr('text-anchor', 'middle')
-        .text('Phone Time')
+        .text('Phone Usage')
         .style("fill", "white");
 
     // fade in effect
-    svg.style('opacity', 0)
-    svg.transition().duration(1000).style('opacity', 1);
+    svg1.style('opacity', 0)
+    svg1.transition().duration(1000).style('opacity', 1);
+
+    svg2.append('g')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+    svg2.append('g')
+        .call(d3.axisLeft(y2));
+
+    svg2.selectAll('.mybar')
+        .data(aggregatedData2)
+        .enter().append('rect')
+        .attr('class', 'mybar')
+        .attr('x', (d) => x(d[0]))
+        .attr('y', (d) => y2(d[1]))
+        .attr('width', x.bandwidth())
+        .attr('height', (d) => height - y2(d[1]))
+        .attr('fill', "#abd9e9");
+
+    // Graph title
+    svg2.append('text')
+        .attr('x', margin.left - 225)
+        .attr('y', margin.top - 80)
+        .attr('text-anchor', 'left')
+        .style('font-size', '16px')
+        .style('fill', 'white')
+        .text('Surveyed Tiredness Based on Phone Reach');
+
+    // Y axis label
+    svg2.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "12em")
+        .style("text-anchor", "middle")
+        .style("fill", "white")
+        .text("Total Tiredness");
+
+    // X axis label
+    svg2.append('text')
+        .attr('x', width / 6 + margin.left/2)
+        .attr('y', height-margin.bottom+150)
+        .attr('text-anchor', 'middle')
+        .text('Phone Reach')
+        .style("fill", "white");
+
+    // fade in effect
+    svg2.style('opacity', 0)
+    svg2.transition().duration(1000).style('opacity', 1);
       
   }, [data]);
   
     return (
         <div>
-          <svg ref={svgRef} width={1000} height={1000}/>
+          <svg ref={svgRef1} width={1500} height={1000}/>
+          <svg ref={svgRef2} width={1000} height={1000}/>
         </div>
     );
 };
